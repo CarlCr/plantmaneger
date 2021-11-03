9f11560ef9bd96d76241e775c7698380ddb004b5
Carlos Garcia <gtxcarl9@gmail.com>
2021-11-03 21:28:19 +0100

    Main app created

--- App.tsx
+++ App.tsx
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect} from 'react';
+import * as Notifications from 'expo-notifications'
+import Routes from './src/routes'
+import AppLoading from 'expo-app-loading'
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+} from '@expo-google-fonts/jost'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  })
+
+  useEffect(() => {
+    async function notifications() {
+     const data =  await Notifications.getAllScheduledNotificationsAsync()
+     console.log("Notificações agendas")
+     console.log(data)
+    }
+  
+    notifications()
+
+  },[])
+
+  if(!fontsLoaded){
+    return (
+      <AppLoading/>
+    )
+  }
+
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Routes/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
